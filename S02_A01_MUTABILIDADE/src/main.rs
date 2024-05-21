5180d660ce74e4be6a2bedc245c962d69a80d1a4
Luciano Paranhos <luciano@paranhos.com.br>
2024-05-21 17:59:48 -0300

    exercicio de mutabilidade de variáveis

---
+++ S02_A01_MUTABILIDADE/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Inicio do programa");
+    let mut x = 5;
+    println!("O valor de x é: {x}");
+
+    x = 6;
+
+    let x = 333;
+    println!("O valor de x agora é: {x}");
+
+    let mut y = 5;
+    println!("O valor de y é: {y}");
+    y = 6;
+    println!("O valor de y agora é: {y}");
+}
